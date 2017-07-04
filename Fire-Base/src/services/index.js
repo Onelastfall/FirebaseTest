36228592243d24ed7a8a542b85ae752b5a04cc23
Yijun Pan Stautland <yijun@staut.land>
2017-07-04 11:38:26 +0200

    Generate feathers services for competencies and persons

--- Fire-Base/src/services/index.js
+++ Fire-Base/src/services/index.js
@@ -1,5 +1,9 @@
 const organizations = require('./organizations/organizations.service.js');
+const persons = require('./persons/persons.service.js');
+const competencies = require('./competencies/competencies.service.js');
 module.exports = function () {
   const app = this; // eslint-disable-line no-unused-vars
   app.configure(organizations);
+  app.configure(persons);
+  app.configure(competencies);
 };
